ab4552d6a81733bd9e6f2abd394abc320bfcfb55
Half_Barba <barbara.sandrolini@gmail.com>
2026-02-14 00:28:40 +0100

    vite.config.js modified

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,13 @@
 import {defineConfig} from "vite";
+import topLevelAwait from "vite-plugin-top-level-await";
 
 export default defineConfig({
-    optimizeDeps: {
-        esbuildOptions: {
-            target: 'esnext'
-        }
-    },
-    build: {
-        target: 'esnext'
-    }
-})
+    plugins: [
+        topLevelAwait({
+            // The export name of top-level await promise for each chunk module
+            promiseExportName: "__tla",
+            // The function to generate import names of top-level await promise in each chunk module
+            promiseImportName: i => `__tla_${i}`
+        })
+    ]
+});
